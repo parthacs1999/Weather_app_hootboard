c8a75d37242e5495d80d9fc6196b48f290292afb
Partha Chakraborty <partha.c1999@gmail.com>
2023-04-14 18:04:03 +0530

    weather details page design completed

--- src/components/Form/form.jsx
+++ src/components/Form/form.jsx
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import classes from './form.module.css';
+import WeatherData from '../WeatherDisplay/WeatherData';
 const Form = () => {
+    const [cityName, setCityName] = useState('');
+    const [goToDataPage, setGoToDataPage] = useState(false);
+    const [getData,setGetData] = useState('');
+    const displayDataHandler = () => {
+        setGoToDataPage(true);
+    }
+    useEffect(()=>{
+        const fetchApi=async()=>{
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid={bbf3b73712491269a21e35f1082714a1}`);
+            const data = await response.json();
+            console.log(data)
+            setGetData(data);
+        };
+        fetchApi();
+    },[cityName])
     return (
         <div className={classes.form__container}>
-            <div className={classes.form__header}>
-                <p>Weather App</p>   
-            </div>
-            <div className={classes.form__bar}></div>
-            <div className={classes.form__body}>
-                <form action="">
-                    <input type="text" placeholder='Enter city name' className={classes.form__input}/>
-                </form>
-            </div>
-            <div className={classes.form__separator}>
-                or
-            </div>
-            <div className={classes.form__button}>
-                <button>Get Device Location</button>
-            </div>
+            {goToDataPage?<WeatherData/>:
+                <div>
+                    <div className={classes.form__header}>
+                        <p>Weather App</p>
+                    </div>
+                    <div className={classes.form__bar}></div>
+                    <div className={classes.form__body}>
+                        <form>
+                            <input type="text" placeholder='Enter city name' className={classes.form__input} onChange={(e) => { setCityName(e.target.value) }} value={cityName} />
+                        </form>
+                    </div>
+                    <div className={cityName !== '' ? '' : classes.hide}>
+                        <ul className={classes.form__data} onClick={displayDataHandler}>
+                            <li>{cityName}</li>
+                            <li>&gt;&gt;&gt;</li>
+                        </ul>
+                    </div>
+                    <div className={classes.form__separator}>
+                        or
+                    </div>
+                    <div className={classes.form__button}>
+                        <button>Get Device Location</button>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
--- src/components/WeatherDisplay/WeatherData.jsx
+++ src/components/WeatherDisplay/WeatherData.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
-
+import classes from './WeatherData.module.css';
+import { IoArrowBackSharp } from 'react-icons/io5';
+import { IoLocationOutline } from 'react-icons/io5';
+import { BsThermometerSun } from 'react-icons/bs';
+import { WiHumidity } from 'react-icons/wi';
 const WeatherData = () => {
   return (
-    <div>WeatherData</div>
+    <div className={classes.data__Container}>
+      <div className={classes.data__header}>
+        <p><IoArrowBackSharp className={classes.back__icon} />{' '}Weather App</p>
+      </div>
+      <div className={classes.data__bar}></div>
+      <div className={classes.display__container}>
+        <div className={classes.display__logo}>
+          <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="logo" />
+        </div>
+        <div className={classes.display__temperature}>
+            <p>13&#8451;</p>
+        </div>
+        <div className={classes.display__weather}>
+          <p>Broken Clouds</p>
+        </div>
+        <div className={classes.display__location}>
+          <p><IoLocationOutline /> Haur,India</p>
+        </div>
+      </div>
+      <div className={classes.data__bar}></div>
+      <div className={classes.data__footer}>
+        <div className={classes.display__feelslike}>
+          <div><BsThermometerSun style={{ color:'#49b3dd',width:'20px',height:'20px'}}/></div>
+          <div className={classes.footer__data}>
+            <p style={{fontSize:'15px'}}>17&#8451;</p>
+            <p>Feels Like</p>
+          </div>
+        </div>
+        <div className={classes.display__humidity}>
+          <div><WiHumidity style={{ color: '#49b3dd' ,width:'20px',height:'20px'}} /></div>
+          <div className={classes.footer__data}>
+            <p style={{fontSize:'15px'}}>84%</p>
+            <p>Humidity</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 

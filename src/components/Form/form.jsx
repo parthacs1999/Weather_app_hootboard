bd0ec2e6793ca8df52b3dc1d836090fca2663b06
Partha Chakraborty <partha.c1999@gmail.com>
2023-04-15 07:52:13 +0530

    documentation of code completed

--- src/components/Form/form.jsx
+++ src/components/Form/form.jsx
@@ -2,26 +2,33 @@ import React, { useState, useEffect } from 'react';
 import classes from './form.module.css';
 import WeatherData from '../WeatherDisplay/WeatherData';
 const Form = () => {
+    // state variables for generating city names,toggling,gettind data from weather api
     const [cityName, setCityName] = useState('');
     const [goToDataPage, setGoToDataPage] = useState(false);
     const [getData, setGetData] = useState(null);
     const[lat,setLat]=useState();
     const[lon,setLon]=useState();
+
+    // function to toogle between pages
     const displayDataHandler = () => {
         setGoToDataPage(!goToDataPage);
     }
+
+    // function to get back to previous page from data showing page
     const getBackHandler=()=>{
         setGoToDataPage(!goToDataPage);
         setCityName('');
     }
 
+    // useEffect for getting latitude and longitude of the device
     useEffect(() => {
         navigator.geolocation.getCurrentPosition((position)=>{
             setLat(position.coords.latitude);
             setLon(position.coords.longitude);
         })
     },[])
-    console.log(lat,lon);
+
+    // useEffect for getting data from weather api with city name
     useEffect(() => {
         const fetchApi = async () => {
             const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=bbf3b73712491269a21e35f1082714a1`);
@@ -31,6 +38,7 @@ const Form = () => {
         fetchApi();
     }, [cityName])
     
+    // useEffect for getting data from weather api with latitude and longitude values
     const fetchApiForOwnLocation=async ()=>{
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=bbf3b73712491269a21e35f1082714a1`);
         const data = await response.json();
@@ -39,17 +47,22 @@ const Form = () => {
     }
     return (
         <div className={classes.form__container}>
+            {/* Toggling pages */}
             {goToDataPage ? <WeatherData data={getData} toggle={getBackHandler}/> :
                 <div>
+                    {/* Header */}
                     <div className={classes.form__header}>
                         <p>Weather App</p>
                     </div>
+                    {/* divider */}
                     <div className={classes.form__bar}></div>
+                    {/* Form container */}
                     <div className={classes.form__body}>
                         <form>
                             <input type="text" placeholder='Enter city name' className={classes.form__input} onChange={(e) => { setCityName(e.target.value) }} value={cityName} />
                         </form>
                     </div>
+                    {/* Error handling logic */}
                     <div className={cityName !== '' ? '' : classes.hide}>
                         {getData === null ? (
                             <ul className={classes.form__data} >
@@ -71,9 +84,11 @@ const Form = () => {
                         }
 
                     </div>
+                    {/* Divider */}
                     <div className={classes.form__separator}>
                         or
                     </div>
+                    {/* Button component */}
                     <div className={classes.form__button}>
                         <button onClick={fetchApiForOwnLocation}>Get Device Location</button>
                     </div>
--- src/components/WeatherDisplay/WeatherData.jsx
+++ src/components/WeatherDisplay/WeatherData.jsx
@@ -7,26 +7,36 @@ import { WiHumidity } from 'react-icons/wi';
 const WeatherData = ({data,toggle}) => {
   return (
     <div className={classes.data__Container}>
+      {/* Main Header */}
       <div className={classes.data__header}>
         <p><IoArrowBackSharp className={classes.back__icon} onClick={()=>toggle()}/>{' '}Weather App</p>
       </div>
+      {/* divider */}
       <div className={classes.data__bar}></div>
+      {/* data display container */}
       <div className={classes.display__container}>
+        {/* Logo coontainer */}
         <div className={classes.display__logo}>
           <img src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="logo" />
         </div>
+        {/* Temperature container */}
         <div className={classes.display__temperature}>
             <p>{data.main.temp}&#8451;</p>
         </div>
+        {/* Weather container */}
         <div className={classes.display__weather}>
           <p>{data.weather[0].description}</p>
         </div>
+        {/* Location container */}
         <div className={classes.display__location}>
           <p><IoLocationOutline /> {data.name},{data.sys.country}</p>
         </div>
       </div>
+      {/* divider */}
       <div className={classes.data__bar}></div>
+      {/* Footer container */}
       <div className={classes.data__footer}>
+        {/* Feels like container */}
         <div className={classes.display__feelslike}>
           <div><BsThermometerSun style={{ color:'#49b3dd',width:'20px',height:'20px'}}/></div>
           <div className={classes.footer__data}>
@@ -34,6 +44,7 @@ const WeatherData = ({data,toggle}) => {
             <p>Feels Like</p>
           </div>
         </div>
+        {/* Humidity container */}
         <div className={classes.display__humidity}>
           <div><WiHumidity style={{ color: '#49b3dd' ,width:'20px',height:'20px'}} /></div>
           <div className={classes.footer__data}>
